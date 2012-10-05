d82be416d895d8455da83bd57ef2625498781009
James Callahan <james@chatid.com>
2012-10-05 19:10:41 +1000

    examples/getkeys: Update to newer method calls

--- examples/getkeys.lua
+++ examples/getkeys.lua
@@ -9,13 +9,9 @@ local dontquit = true
 local addrinfo = dns.lookup("127.0.0.1",6379)
 local sock = socket.new_tcp ( addrinfo.ai_family )
 sock:connect ( addrinfo , dispatch , function ( sock , err )
-		if not sock then
-			print ( "ERROR Connecting: " .. err )
-			dontquit = false
-			return
-		end
-		local redis_cmd = redis.add_sock ( dispatch , sock )
-		redis_cmd ( function ( reply , err )
+		assert ( sock , err )
+		local red = redis.add_sock ( dispatch , sock )
+		red:query ( function ( reply , err )
 				for i , v in ipairs ( reply ) do
 					print ( v )
 				end
